d2457daef07a28eeb48ed8b7b7d102f1254e67cb
arcnmx <git@git.arcn.mx>
2024-05-12 13:49:27 -0700

    flake update

--- flake.nix
+++ flake.nix
@@ -16,6 +16,37 @@
           path = "${cozette}/share/fonts/misc/cozette.bdf";
         }
       ];
+      esphome = { nixpkgs'esphome, python3Packages }: let
+        pythonOverlay = pythonPackages: super: {
+          esphome-dashboard = super.esphome-dashboard or (pythonPackages.callPackage (nixpkgs.outPath + "/pkgs/tools/misc/esphome/dashboard.nix") { });
+        };
+        python = python3Packages.python.override {
+          packageOverrides = nixlib.composeManyExtensions [
+            pythonOverlay
+          ];
+        };
+        overrides'attrs = old: {
+          # XXX: upstream nixpkgs esphome is currently broken due to pillow version mismatch...
+          # see: https://github.com/NixOS/nixpkgs/pull/311160
+          pillowVersion = python3Packages.pillow.version;
+          postPatch = ''
+            cat requirements_optional.txt >> requirements.txt
+            substituteInPlace esphome/components/font/__init__.py \
+              --replace-fail "10.2.0" "$pillowVersion"
+          '' + old.postPatch or "";
+          propagatedBuildInputs = with python.pkgs; old.propagatedBuildInputs ++ [
+            cairosvg
+          ];
+        };
+        # esphome overrides `packageOverrides` in a dumb way .-.
+        overrides = {
+          python3Packages = python.pkgs // {
+            python = python // {
+              override = _: python;
+            };
+          };
+        };
+      in (nixpkgs'esphome.override overrides).overridePythonAttrs overrides'attrs;
       flash-espresense = { writeShellScriptBin, esptool, jq, curl }: let
         bins = [ esptool jq curl ];
       in writeShellScriptBin "flash-espresense" ''
